e49d4a94bcec973b89e2ff0387f424a6bf817a92
Mohamed Refaat <refaatsilva21@gmail.com>
2020-12-23 17:34:23 +0200

    first commit

---
+++ JS/main.js
@@ -0,0 +1,80 @@
+var empolyees =
+[
+    {name:"ahmed",age:"25",salary:"6000",phone:"010155545"},
+    {name:"soha",age:"20",salary:"6500",phone:"0109999545"},
+    {name:"omar",age:"15",salary:"69990",phone:"010045545"},
+    {name:"sameh",age:"66",salary:"6000",phone:"010155545"},
+    {name:"merna",age:"70",salary:"6000",phone:"010155545"},
+]
+
+var addbtn=document.getElementById("addbtn");
+var nameInput=document.getElementById("name");
+var ageInput=document.getElementById("age");
+var salaryInput=document.getElementById("salary");
+var phoneInput=document.getElementById("phone");
+var inputs=document.getElementsByClassName("form-control");
+var empolyees=[];
+addbtn.onclick=function() {
+    addemployee();
+    displaydata();
+    resetform()
+}
+  
+
+    function addemployee(){
+    var empolyee=
+    {
+       name:nameInput.value,
+       age:ageInput.value,
+       salary:salaryInput.value,
+       phone:phoneInput.value,
+   }
+   empolyees.push(empolyee);
+   localStorage.setItem("employeeslist" , JSON.stringify (empolyee))
+    }
+
+    function displaydata(){
+    var box="";
+    for( var i=0;i<empolyees.length;i++)
+    {
+        box+=`
+        <tr>
+        <td>${empolyees[i].name}</td>
+        <td>${empolyees[i].age}</td>
+        <td>${empolyees[i].salary}</td>
+        <td>${empolyees[i].phone}</td>
+        <td> <button onclick='deleteemployee(${i})' class="btn btn-danger"> delete</button> </td>
+        </tr>
+        `
+    }
+    document.getElementById("tablebody").innerHTML=box
+}
+
+function resetform() {
+    for (var i= 0; i<inputs.length; i++) {
+        inputs[i].value=""
+    }
+}
+function deleteemployee(index){
+    empolyees.splice(index,1);
+    displaydata()
+
+}
+
+function search(searchtxt) {
+    var box="";
+    for( var i=0;i<empolyees.length;i++)
+    if(empolyees[i].name.toLowerCase().includes(searchtxt.toLowerCase()))
+    {
+        box+=`
+        <tr>
+        <td>${empolyees[i].name}</td>
+        <td>${empolyees[i].age}</td>
+        <td>${empolyees[i].salary}</td>
+        <td>${empolyees[i].phone}</td>
+        <td> <button onclick='deleteemployee(${i})' class="btn btn-danger"> delete</button> </td>
+        </tr>
+        `
+    }
+    document.getElementById("tablebody").innerHTML=box
+}
